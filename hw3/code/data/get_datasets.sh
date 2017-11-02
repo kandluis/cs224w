d0d2d0068a8d5da619baf80fdfc9afa1021962db
Luis Perez <luis0@stanford.edu>
2017-11-01 23:32:21 -0700

    Start HW3 Q3

--- hw3/code/data/get_datasets.sh
+++ hw3/code/data/get_datasets.sh
@@ -1,1 +1,5 @@
 curl http://web.stanford.edu/class/cs224w/homeworks/hw3/data_sets/thresholds.txt -o thresholds.txt -s
+curl http://cs224w.stanford.edu/homeworks/hw3/data_sets/imdb_actor_edges.tsv -o imdb_actor_edges.tsv -s
+curl http://cs224w.stanford.edu/homeworks/hw3/data_sets/imdb_actors_key.tsv -o imdb_actors_key.tsv -s
+curl http://cs224w.stanford.edu/homeworks/hw3/data_sets/SIR_erdos_renyi.txt -o SIR_erdos_renyi.txt -s
+curl http://cs224w.stanford.edu/homeworks/hw3/data_sets/SIR_preferential_attachment.txt -o SIR_preferential_attachment.txt -s
